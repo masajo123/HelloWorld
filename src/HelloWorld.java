a367065566511348402dbdd8a1a0e5944088c137
unknown <martina@zid-nb-33.tugraz.local>
2022-09-16 19:06:47 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    //psvm+TAB oder main
+    public static void main(String[] args) {
+
+        //sout
+        System.out.println("Hello World :)");
+        System.out.println("Welcome to KONF 2022");
+        System.out.println("Buonas Dias");
+        System.out.println("Va bene");
+
+
+
+    }
+}
